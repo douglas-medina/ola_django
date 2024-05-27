b7834ac65460a2f637a043bc665b97a30de8fb53
Douglas Medina <medina.douglas.dmv@gmail.com>
2024-05-27 17:05:48 -0300

    html, render, template, installed_apps e templatedoesntexist

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.
 def blog(request):
     print('Página do blog')
-    return HttpResponse('Blog do app 1')
+    return render(
+        request,
+        'blog/index.html',
+    )
 
 def exemplo(request):
     print('Página do exemplo')
-    return HttpResponse('exemplo do app 1')
+    return render(
+        request,
+        'blog/exemplo.html',
+    )
